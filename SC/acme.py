de654e126a133b4c6d9ff51bb5cd4926c2c55338
Michael Curry <mdcurry1138@gmail.com>
2019-09-06 11:33:50 -0400

    adds acme Product class

---
+++ SC/acme.py
@@ -0,0 +1,16 @@
+import random
+
+class Product:
+    '''This is my Description.
+
+    Maybe I should come back to this when I'm done?
+    '''
+
+    def __init__(self,name=None, price=10, weight=20, flammability=0.5,
+                 )
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.flammability = flammability
+        self.identifier = random.randint(1000000, 9999999)
+
